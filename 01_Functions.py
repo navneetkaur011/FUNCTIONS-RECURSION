337f0fc246f9b87bc37d718270ec0062b4fadf1b
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-24 23:49:11 +0530

    Add functions implementation file

---
+++ 01_Functions.py
@@ -0,0 +1,20 @@
+print("---FUNCTIONS---")
+# Function is a block of code that performs a specific task.
+def diff(a,b):  #Parameters
+    d = a-b 
+    print(d)
+    return d  #These line of code will not return any output.
+
+diff(456,71)  # function call: arguments
+diff(98230,6103)
+
+def py():  # function with no parameters 
+    print("\nFunctions in Python")
+
+py()
+
+def nothing():   
+    print("\nNothing to do.")
+
+print(nothing()) # this will print None
+
