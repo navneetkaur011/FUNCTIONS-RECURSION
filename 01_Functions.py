6dacd38b6a9c1bb33b6524d0d4f78d4c33e298eb
Navneet Kaur <navvkaurnanda@gmail.com>
2026-07-27 23:32:31 +0530

    Add functions to print elements of a list.

--- 01_Functions.py
+++ 01_Functions.py
@@ -48,5 +48,16 @@ def cal_prod(a=1, b=2):
 cal_prod()  # Uses default values
 cal_prod(5) # Uses a=5, b=2
 
+# Function to print elements of a list in a single line
 
+cities = ["Delhi", "Gurgaon", "Noida", "Mumbai", "Pune"]
+heroes = ["thor", "ironman", "captain america", "batman"]
 
+def print_len(list_item):
+    print("Total items:", len(list_item))
+    for item in list_item:
+        print(item, end=" ")
+    print() # New line
+
+print_len(cities)
+print_len(heroes)
