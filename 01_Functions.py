53370dd6ed679a97daa60656601c96b3d25eeb62
navneetkaur011 <navvkaurnanda@gmail.com>
2026-08-03 22:57:13 +0530

    Update the functions

--- 01_Functions.py
+++ 01_Functions.py
@@ -4,40 +4,41 @@ def diff(a,b):  #Parameters
     d = a-b 
     print(d)
     return d  #These line of code will not return any output.
-
+print("--Difference using functions-- ")
 diff(456,71)  # function call: arguments
 diff(98230,6103)
 
+print("\n--Function with no parameters--")
 def py():  # function with no parameters 
-    print("\nFunctions in Python")
-
+    print("Functions in Python")
 py()
 
+print("\n--None function--")
 def nothing():   
-    print("\nNothing to do.")
-
+    print("Nothing to do.")
 print(nothing()) # this will print None
 # End of basic function examples.
 
+print("\n--Function to calculate the square of a number--")
 def square(num):
     return num * num # Note: Always remember to use return statements when you need to store function output.
-
 print(square(4))
 print(square(256))
 
+print("\n--Function to calculate the average--")
 # Function to calculate the average of 3 numbers
 def calc_avg(a, b, c):
     sum_val = a + b + c
     avg = sum_val / 3
     print("The average is:", avg)
     return avg
-
 calc_avg(98, 97, 95)
 
+ 
 # Demonstrating built-in functions vs user-defined functions
 
 # Built-in functions example
-print("Length of string:", len("ApnaCollege"))
+print("\nLength of string:", len("ApnaCollege"))
 print("Type example:", type(10))
 
 # Default arguments example taught in Apna College
