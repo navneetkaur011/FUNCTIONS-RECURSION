91144ea107d32898e437319a2df9b3ff95b19b30
navneetkaur011 <navvkaurnanda@gmail.com>
2026-08-04 23:11:04 +0530

    Update the functions

--- 01_Functions.py
+++ 01_Functions.py
@@ -2,14 +2,14 @@ print("---FUNCTIONS---")
 # Function is a block of code that performs a specific task.
 def diff(a,b):  #Parameters
     d = a-b 
-    print(d)
+    print("The difference of", a, "and", b, "is:", d)
     return d  #These line of code will not return any output.
 print("--Difference using functions-- ")
 diff(456,71)  # function call: arguments
 diff(98230,6103)
 
 print("\n--Function with no parameters--")
-def py():  # function with no parameters 
+def py():
     print("Functions in Python")
 py()
 
@@ -30,35 +30,19 @@ print("\n--Function to calculate the average--")
 def calc_avg(a, b, c):
     sum_val = a + b + c
     avg = sum_val / 3
-    print("The average is:", avg)
+    print("The average of", a,",", b, "and", c, "is:", avg)
     return avg
 calc_avg(98, 97, 95)
 
- 
+print("\n--Built-in functions--") 
 # Demonstrating built-in functions vs user-defined functions
+print("Length of string:", len("Built-in Functions"))
+print("Type:", type(10))
 
-# Built-in functions example
-print("\nLength of string:", len("ApnaCollege"))
-print("Type example:", type(10))
-
-# Default arguments example taught in Apna College
-def cal_prod(a=1, b=2):
-    print("Product is:", a * b)
+print("\n--Default Parameters--")
+#Assigning a default value to parameter, which is used when no argument is passed.
+def product(a=18, b=2):
+    print("Product of", a, "and", b, "is:", a * b)
     return a * b
-
-cal_prod()  # Uses default values
-cal_prod(5) # Uses a=5, b=2
-
-# Function to print elements of a list in a single line
-
-cities = ["Delhi", "Gurgaon", "Noida", "Mumbai", "Pune"]
-heroes = ["thor", "ironman", "captain america", "batman"]
-
-def print_len(list_item):
-    print("Total items:", len(list_item))
-    for item in list_item:
-        print(item, end=" ")
-    print() # New line
-
-print_len(cities)
-print_len(heroes)
+product()  # Uses default values
+product(5) # Uses a=5, b=2
